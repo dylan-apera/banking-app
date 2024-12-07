bcae7919690e4a0ae4c8aa8a5948c17e1759174d
dylan-apera <74391707+dylan-apera@users.noreply.github.com>
2024-12-08 00:38:43 +1300

    Add: DB for testing

--- api/api.py
+++ api/api.py
@@ -1,9 +1,28 @@
 from flask import Flask, request, jsonify
-
 app = Flask(__name__)
+from flask_sqlalchemy import SLQAlchemy
 
-@app.route("/add-money", methods=["POST"])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(90), unique=True, nullable=False)
+    balance = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"{self.name} - {self.balance}"
+
+
+@app.route('/balance')
+def get_balance():
+    return balance
+
+@app.route('/add-money', methods=['POST'])
 def add_money():
-    return 0
 
-#To be continued
+    return 
+
+if __name__ == "__main__":
+    app.run(debug=True)
